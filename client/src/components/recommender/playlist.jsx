0228a1a41254916822a17c0bb4b40719667d64c4
Evan Greenstein <evanlg16@gmail.com>
2023-01-21 16:21:14 -0500

    Got the api calls to flask server to work

---
+++ client/src/components/recommender/playlist.jsx
@@ -0,0 +1,5 @@
+const Playlist = ({ trackIDS }) => {
+   return <div>{JSON.stringify(trackIDS)}</div>
+}
+
+export default Playlist
---
+++ client/src/utils/api.js
@@ -0,0 +1,25 @@
+export default async function fetchSpotAIData(
+   sliderValues,
+   settings,
+   setTrackIDS
+) {
+   let requestURL = 'http://127.0.0.1:5000/recommend_tracks/?'
+   for (let index = 1; index < sliderValues.length; index++) {
+      const { step, key } = settings[index]
+      requestURL += `${key}=${
+         step
+            ? sliderValues[index]
+            : ((1.0 * sliderValues[index]) / 100).toFixed(3)
+      }&`
+   }
+   requestURL += `${settings[0].key[0]}=${sliderValues[0][0]}&${settings[0].key[1]}=${sliderValues[0][1]}`
+   console.log(requestURL)
+   let response = await fetch(requestURL)
+      .then((res) => {
+         return res.json()
+      })
+      .catch((err) => console.error(err))
+
+   console.log(response)
+   setTrackIDS(response.res)
+}
