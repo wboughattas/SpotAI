ce2ea8bb1061379d904603f0c6a70f8cb3d1e503
wboughattas <wboughattas@outlook.com>
2023-01-21 18:28:29 -0500

    added query

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,11 +1,12 @@
-from flask import Flask, request
+import itertools
+
+from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
 from flasgger import Swagger
 import os
 from dotenv import load_dotenv
-from __init__ import ROOT_DIR
 
-load_dotenv(os.path.join(ROOT_DIR, 'react-flask-sqlite', 'client', 'node_modules', '.env'))
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SWAGGER'] = {'ui_params': {'displayRequestDuration': 'true'}, }
@@ -142,7 +143,44 @@ def playlists():
     max_year = request.args.get('max_year')
 
     # mysql
-    print('test')
+    cursor = mysql.connection.cursor()
+    query = f'''
+        SELECT t.id
+        FROM (SELECT t.id,
+                     100 - 100 * SQRT(0
+                                      {f' + (tempo - {tempo}) * (tempo - {tempo})' if tempo else ''}
+                                      {f' + (danceability - {danceability}) * (danceability - {danceability})' if danceability else ''}
+                                      {f' + (energy - {energy}) * (energy - {energy})' if energy else ''}
+                                      {f' + (loudness - {loudness}) * (loudness - {loudness})' if loudness else ''}
+                                      {f' + (mode - {mode}) * (mode - {mode})' if mode else ''}
+                                      {f' + (speechiness - {speechiness}) * (speechiness - {speechiness})' if speechiness else ''}
+                                      {f' + (acousticness - {acousticness}) * (acousticness - {acousticness})' if acousticness else ''}
+                                      {f' + (instrumentalness - {instrumentalness}) * (instrumentalness - {instrumentalness})' if instrumentalness else ''}
+                                      {f' + (liveness - {liveness}) * (liveness - {liveness})' if liveness else ''}
+                                      {f' + (valence - {valence}) * (valence - {valence})' if valence else ''}) as score
+              FROM track t
+              {f'LEFT JOIN album a on album_id = a.id' if max_year and min_year else ''}
+              {f' WHERE popularity > {popularity}' if popularity else ''}
+                AND t.name NOT REGEXP '.*[?].*$'
+                AND t.id != ''
+                {f' AND a.release_date < {max_year} AND a.release_date > {min_year}' if max_year and min_year else ''}
+              ORDER BY score DESC
+              {f'LIMIT {int(accuracy * (10 ** 7))} ' if accuracy else ''}) t
+                 LEFT JOIN track_playlist1 t_p on t.id = t_p.track_id
+                 LEFT JOIN playlist p on p.id = t_p.playlist_id
+        GROUP BY t.id
+        {f'HAVING SUM(p.followers) > {min_follower_count}' if min_follower_count else ''}
+        ORDER BY score DESC
+        {f'LIMIT {top_n}' if top_n else ''}     
+    '''
+    cursor.execute(query)
+    response = cursor.fetchall()
+    response = list(itertools.chain(*response))
+    return jsonify({
+        'res': response,
+        'status': 200,
+        'msg': 'Success'
+    })
 
 
 if __name__ == "__main__":
